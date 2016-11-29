6dfba8db9e25fd3218edbd3a2c640ae31020b685
lucasmonteiroi <zogru@outlook.com>
2016-11-29 20:37:29 -0200

    Alteracoes de arquivos

--- 02_Desenvolvimento/sis-cine/menu.c
+++ 02_Desenvolvimento/sis-cine/menu.c
@@ -1,6 +1,5 @@
 #include "stdio.h"
 #include "conio.h"
-#include "locale.h"
 
 void exibirBoasVindas(){
     printf("|-------------------------------------------|\n");
--- 02_Desenvolvimento/sis-cine/sessoes.c
+++ 02_Desenvolvimento/sis-cine/sessoes.c
@@ -1,6 +1,5 @@
 #include "stdio.h"
 #include "conio.h"
-#include "locale.h"
 
 struct pagamentos{
     int cont;
